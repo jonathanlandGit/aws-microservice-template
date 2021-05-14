68abe312ad74f92d53964a7c93ce893440c0e4ba
Jonathan Land <jonathanland.git@gmail.com>
2021-05-14 14:47:11 -0500

    jenkins test

--- src/main/java/com/mycompany/microservicestemplate/repository/GreetingsRepository.java
+++ src/main/java/com/mycompany/microservicestemplate/repository/GreetingsRepository.java
@@ -31,6 +31,7 @@ public class GreetingsRepository {
         greetings.add(new Greeting("Bonjour, ", "Wish all your friends good morning!"));
         greetings.add(new Greeting("Hola, ", "Think of all the people who make you happy!"));
         greetings.add(new Greeting("Hey man , ", "Just keep on keeping on!"));
+//        greetings.add(new Greeting("Hey man , ", "Just keep on keeping on!"));
 
         LOGGER.info("Populated greetings {}", greetings);
     }
